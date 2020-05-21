81852b696e740d2728579a0fc00bb0f8cde6d93e
TBoileau <t-boileau@email.com>
2020-05-21 12:56:19 +0200

    feat(lib): add factory
    Add factory injection

--- src/Definition.php
+++ src/Definition.php
@@ -3,6 +3,7 @@
 namespace TBoileau\DependencyInjection;
 
 use ReflectionClass;
+use ReflectionMethod;
 use ReflectionParameter;
 
 /**
@@ -19,17 +20,22 @@ class Definition
     /**
      * @var bool
      */
-    private bool $shared = true;
+    private bool $shared;
 
     /**
      * @var array
      */
-    private array $aliases = [];
+    private array $aliases;
+
+    /**
+     * @var ReflectionMethod|null
+     */
+    private ?ReflectionMethod $factory;
 
     /**
      * @var Definition[]
      */
-    private array $dependencies = [];
+    private array $dependencies;
 
     /**
      * @var ReflectionClass
@@ -42,13 +48,21 @@ class Definition
      * @param bool $shared
      * @param array $aliases
      * @param array $dependencies
+     * @param null|ReflectionMethod $factory
+     * @throws \ReflectionException
      */
-    public function __construct(string $id, bool $shared = true, array $aliases = [], array $dependencies = [])
-    {
+    public function __construct(
+        string $id,
+        bool $shared = true,
+        array $aliases = [],
+        array $dependencies = [],
+        ?ReflectionMethod $factory = null
+    ) {
         $this->id = $id;
         $this->shared = $shared;
         $this->aliases = $aliases;
         $this->dependencies = $dependencies;
+        $this->factory = $factory;
         $this->class = new ReflectionClass($id);
     }
 
@@ -77,22 +91,32 @@ class Definition
      */
     public function newInstance(ContainerInterface $container): object
     {
+        if ($this->factory !== null) {
+            $this->factory->invokeArgs(null, $this->resolve($container, $this->factory));
+        }
+
         $constructor = $this->class->getConstructor();
 
         if (null === $constructor) {
             return $this->class->newInstance();
         }
 
-        $parameters = $constructor->getParameters();
+        return $this->class->newInstanceArgs($this->resolve($container, $constructor));
+    }
 
-        return $this->class->newInstanceArgs(
-            array_map(function (ReflectionParameter $param) use ($container) {
-                if ($param->getClass() === null) {
-                    return $container->getParameter($param->getName());
-                }
+    /**
+     * @param ContainerInterface $container
+     * @param ReflectionMethod $method
+     * @return array
+     */
+    private function resolve(ContainerInterface $container, ReflectionMethod $method): array
+    {
+        return array_map(function (ReflectionParameter $param) use ($container) {
+            if ($param->getClass() === null) {
+                return $container->getParameter($param->getName());
+            }
 
-                return $container->get($param->getClass()->getName());
-            }, $parameters)
-        );
+            return $container->get($param->getClass()->getName());
+        }, $method->getParameters());
     }
 }
