7720f599f53729ae74b4c52c5b9a25667015f404
TBoileau <t-boileau@email.com>
2020-05-21 11:13:59 +0200

    refactor(test): refactor unit tests
    Refactor unit tests by renaming with non breaking name.

--- tests/ContainerTest.php
+++ tests/ContainerTest.php
@@ -5,7 +5,7 @@ namespace TBoileau\DependencyInjection\Tests;
 use PHPUnit\Framework\TestCase;
 use Psr\Container\NotFoundExceptionInterface;
 use TBoileau\DependencyInjection\Container;
-use TBoileau\DependencyInjection\NotFoundException;
+use TBoileau\DependencyInjection\Tests\Fixtures\Bar;
 use TBoileau\DependencyInjection\Tests\Fixtures\Database;
 use TBoileau\DependencyInjection\Tests\Fixtures\Foo;
 use TBoileau\DependencyInjection\Tests\Fixtures\Router;
@@ -17,49 +17,73 @@ use TBoileau\DependencyInjection\Tests\Fixtures\RouterInterface;
  */
 class ContainerTest extends TestCase
 {
-    public function test()
+    public function test if shared service is found()
     {
         $container = new Container();
+        $foo1 = $container->get(Foo::class);
+        $this->assertInstanceOf(Foo::class, $foo1);
+        $foo2 = $container->get(Foo::class);
+        $this->assertEquals(spl_object_id($foo1), spl_object_id($foo2));
+    }
 
-        $container->addAlias(RouterInterface::class, Router::class);
-
+    public function test if non shared service is found()
+    {
+        $container = new Container();
         $container->getDefinition(Foo::class)->setShared(false);
+        $foo1 = $container->get(Foo::class);
+        $this->assertInstanceOf(Foo::class, $foo1);
+        $foo2 = $container->get(Foo::class);
+        $this->assertNotEquals(spl_object_id($foo1), spl_object_id($foo2));
+    }
 
+    public function test if service with injection is found()
+    {
+        $container = new Container();
+        $bar = $container->get(Bar::class);
+        $this->assertInstanceOf(Bar::class, $bar);
+        $this->assertInstanceOf(Foo::class, $bar->foo);
+    }
+
+    public function test if service with parameters is found()
+    {
+        $container = new Container();
         $container
             ->addParameter("dbUrl", "url")
             ->addParameter("dbName", "name")
             ->addParameter("dbUser", "user")
             ->addParameter("dbPassword", "password")
         ;
-
-        $database1 = $container->get(Database::class);
-        $database2 = $container->get(Database::class);
-        $this->assertInstanceOf(Database::class, $database1);
-        $this->assertInstanceOf(Database::class, $database2);
-        $this->assertEquals(spl_object_id($database1), spl_object_id($database2));
-        $this->assertInstanceOf(Router::class, $container->get(RouterInterface::class));
-
-        $router1 = $container->get(RouterInterface::class);
-        $router2 = $container->get(RouterInterface::class);
-        $this->assertEquals(spl_object_id($router1), spl_object_id($router2));
-
-        $foo1 = $container->get(Foo::class);
-        $foo2 = $container->get(Foo::class);
-        $this->assertNotEquals(spl_object_id($foo1), spl_object_id($foo2));
-
+        $database = $container->get(Database::class);
+        $this->assertInstanceOf(Database::class, $database);
     }
 
-    public function testIfParameterNotFound()
+    public function test if service alias is found()
+    {
+        $container = new Container();
+        $container->addAlias(RouterInterface::class, Router::class);
+        $router = $container->get(RouterInterface::class);
+        $this->assertInstanceOf(RouterInterface::class, $router);
+        $this->assertInstanceOf(Router::class, $router);
+    }
+
+    public function test if parameter is found()
+    {
+        $container = new Container();
+        $container->addParameter("key", "value");
+        $this->assertEquals("value", $container->getParameter("key"));
+    }
+
+    public function test if parameter not found()
     {
         $container = new Container();
         $this->expectException(NotFoundExceptionInterface::class);
         $container->getParameter("fail");
     }
 
-    public function testIfClassNotFound()
+    public function test if service not found()
     {
         $container = new Container();
         $this->expectException(NotFoundExceptionInterface::class);
-        $container->get(Bar::class);
+        $container->get(Fail::class);
     }
 }
